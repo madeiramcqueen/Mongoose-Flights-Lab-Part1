0f81df7bfaee3775ec656c0304026dd8d94f0ec2
madeiramcqueen <madeira@mcqueen.net>
2022-10-29 13:55:51 -0700

    Add datetime-local to new flight page

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,12 +2,25 @@ const Flight = require("../models/flight")
 
 module.exports = {
     new: newFlight,
-    index,
     create,
+    index,
 };
 
 function newFlight(req, res) {
     res.render("flights/new");
+}
+
+function create(req, res) {
+    const flight = new Flight(req.body);
+    // save flight into database
+    flight.save(function (err) {
+        // if we don't redirect, the new page will be shown
+        // with /flights in the address bar
+        if (err) return res.redirect("/flights/new");
+        console.log(flight);
+        // for now, redirect back to new.ejs
+        res.redirect("/flights");
+    });
 }
 
 function index(req, res) {
@@ -18,24 +31,5 @@ function index(req, res) {
             res.redirect("/");
         }
         res.render("flights/index", { flights });
-    });
-}
-
-function create(req, res) {
-    // convert nowShowing's checkbox of nothing or "on" to boolean
-    req.body.nowShowing = !!req.body.nowShowing;
-    // remove any whitespace at start and end of cast
-    req.body.cast = req.body.cast.trim();
-    // split cast into an array if it's not an empty string - using a regular expression as a separator
-    if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
-    const flight = new Flight(req.body);
-    // save movie into database
-    flight.save(function (err) {
-        // if we don't redirect, the new page will be shown
-        // with /flights in the address bar
-        if (err) return res.redirect("/flights/new");
-        console.log(flight);
-        // for now, redirect back to new.ejs
-        res.redirect("/flights");
     });
 }
