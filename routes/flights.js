8646dc7f10d0f917638d5a80d43442a3d1a80585
madeiramcqueen <madeira@mcqueen.net>
2022-10-30 14:50:04 -0700

    Add validations and debug errors

--- routes/flights.js
+++ routes/flights.js
@@ -2,18 +2,13 @@ var express = require('express');
 var router = express.Router();
 var flightsCtrl = require('../controllers/flights');
 
-// /* GET flights listing. */
-// router.get('/', function (req, res, next) {
-//   res.send('respond with a resource');
-// });
-
 /* GET all flights listings. */
 router.get('/', flightsCtrl.index)
 
 /* POST new flight. */
 router.post('/', flightsCtrl.create);
 
-/* GET new movies listing. */
+/* GET new flights listing. */
 router.get('/new', flightsCtrl.new);
 
 module.exports = router;
