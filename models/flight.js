fac30f66f62f6b450dbbb59f0db48363e60bc02f
madeiramcqueen <madeira@mcqueen.net>
2022-10-31 14:32:13 -0700

    Add destinationSchema

--- models/flight.js
+++ models/flight.js
@@ -2,11 +2,17 @@ const mongoose = require('mongoose');
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
 
+const destinationSchema = new Schema({
+    airport: String,
+    arrival: Date
+})
+
 const flightSchema = new Schema({
     airline: String,
     airport: String,
     flightNo: Number,
     departs: Date,
+    destinations: [destinationSchema]
 });
 
 // compile schema into model and export it
