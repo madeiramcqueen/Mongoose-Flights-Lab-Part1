0cbbe9c8c06545454dff960d4b081343a311c24d
madeiramcqueen <madeira@mcqueen.net>
2022-11-04 17:16:10 -0700

    Clean up code

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -17,12 +17,6 @@ function create(req, res) {
     req.body.flight = req.params.id
     const ticket = new Ticket(req.body);
     ticket.save(function (err) {
-        // if (err) return res.redirect('tickets/new');
-        //test to see created ticket
-        console.log(ticket);
         res.redirect(`/flights/${req.params.id}`)
-        //get the flight by id, callback required
-
-
     });
 }
--- models/ticket.js
+++ models/ticket.js
@@ -7,6 +7,6 @@ const ticketSchema = new Schema({
     seat: { type: String, match: /[A-F][1-9]\d?/ },
     price: { type: Number },
     flight: { type: Schema.Types.ObjectId, ref: 'Flight' },
-})
+});
 
 module.exports = mongoose.model("Ticket", ticketSchema);
